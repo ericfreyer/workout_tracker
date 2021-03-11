3f6c718e5387ef9bb8aa66da26cc4e465c36266e
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-03-11 16:17:29 -0500

    added models

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [{
+    type: {
+      type: String,
+      trim: true,
+      required: 'Please enter a value.'
+    },
+    name: {
+      type: String,
+      required: 'You must enter an exercise name.'
+    },
+    duration: {
+      type: Number
+    },
+    weight: {
+      type: Number
+    },
+    reps: {
+      type: Number
+    },
+    sets: {
+      type: Number
+    },
+    distance: {
+      type: Number
+    }
+  }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
